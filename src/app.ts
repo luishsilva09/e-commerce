2fbeb5c6c9152fe91fbb57af0341f51e9641d27f
Luís Henrique <luishsilva09@gmail.com>
2023-01-24 21:20:46 -0300

    feat: use routes on app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import cors from "cors";
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(cors(), express.json());
+
+app.use(routes);
 
 export default app;
